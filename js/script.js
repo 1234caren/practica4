b926afde2778610f4b97d1e26b52d0fe6a117462
caren <caren.ruiz@ecloudsolutions.com>
2017-12-01 10:51:38 -0300

    modificado el newsletter

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function(){
+
+});
+
+function newsLeave(sender, defaultValue) {
+    if (jQuery.trim(sender.value) == '') sender.value = defaultValue;
+}
+function newsSelect(sender, defaultValue) {
+    if (sender.value == defaultValue) sender.value = '';
+    else sender.select();
+}
