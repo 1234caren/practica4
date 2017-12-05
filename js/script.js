abaa441c063f393f69995570368429914ed8328d
caren <caren.ruiz@ecloudsolutions.com>
2017-12-05 13:31:50 -0300

    grilla producto final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
 jQuery(document).ready(function(){
-
+	jQuery('.lista-uno').on('click', function(){
+		jQuery('.lista-dos',this).slideToggle();
+		
+	});
 });
+
 
 function newsLeave(sender, defaultValue) {
     if (jQuery.trim(sender.value) == '') sender.value = defaultValue;
